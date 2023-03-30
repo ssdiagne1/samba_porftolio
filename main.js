43fd471348e33d3fe16155955d1890856841b496
ssdiagne1 <diagnefk@gmail.com>
2023-03-30 02:55:37 -0400

    omega_version_4.3

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ var typed = new Typed('.typer', {
     backSpeed: 90,
     loop: true
 });
+
+let valueNumbers = document.querySelectorAll(".exp-number");
+let interval = 30;
+
+valueNumbers.forEach((valueNumber)=>{
+    let startValue = 0;
+    let endValue = parseInt(valueNumber.getAttribute("data-val"));
+    let duration = Math.floor(interval/endValue);
+    let counter = setInterval(()=>{
+        startValue +=1;
+        valueNumber.textContent = startValue;
+        if(startValue == endValue){
+            clearInterval(counter);
+        }
+    },duration)
+});
